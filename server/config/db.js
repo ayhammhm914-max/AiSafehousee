c17bfa40077e0e479a1ec8dc67a70508819a6f6c
abood sharef <ayham mohammed>
2026-04-13 00:07:49 +0300

    fix db config

--- server/config/db.js
+++ server/config/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host:     process.env.DB_HOST,
-  user:     process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host:     process.env.MYSQLHOST     || process.env.DB_HOST,
+  user:     process.env.MYSQLUSER     || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASS,
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
+  port:     process.env.MYSQLPORT     || 3306,
 });
 
 module.exports = pool.promise();
